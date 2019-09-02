10ccec7296e39058dcfaa0e98e369f9807ea9ff7
bbennett <tbbennett1@gmail.com>
2019-09-02 16:35:37 -0700

    Separated Login and SignUp Form Comps

--- frontend/components/session_form/login_form_container.jsx
+++ frontend/components/session_form/login_form_container.jsx
@@ -2,19 +2,19 @@ import { connect } from 'react-redux';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { login } from '../../actions/session_actions';
-import SessionForm from './session_form';
+import SessionForm from './login_form';
 
 const mapStateToProps = ({ errors }) => {
   return {
     errors: errors.session,
-    formType: 'login',
+    // formType: 'login',
     navLink: <Link to="/signup">Don't have an account?</Link>,
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    processForm: (user) => dispatch(login(user)),
+    login: (user) => dispatch(login(user)),
   };
 };
 
--- frontend/components/session_form/signup_form_container.jsx
+++ frontend/components/session_form/signup_form_container.jsx
@@ -2,19 +2,19 @@ import { connect } from 'react-redux';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { signup } from '../../actions/session_actions';
-import SessionForm from './session_form';
+import SessionForm from './login_form';
 
 const mapStateToProps = ({ errors }) => {
   return {
     errors: errors.session,
-    formType: 'signup',
+    // formType: 'signup',
     navLink: <Link to="/signin">Already have a Clonebase account? Log in</Link>,
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    processForm: (user) => dispatch(signup(user)),
+    signup: (user) => dispatch(signup(user)),
   };
 };
 
