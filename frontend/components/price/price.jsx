a57d4d391e2893f86c728f03490b7784968aff4d
bbennett <tbbennett1@gmail.com>
2019-09-05 16:34:36 -0700

    styling price index

--- frontend/components/price/price.jsx
+++ frontend/components/price/price.jsx
@@ -7,70 +7,72 @@ class Price extends React.Component{
       <div className="price-index">
         <Heading />
         <div className="price-top">
-          <div className="search-box">
-            <section>
-              <div>
-                <svg></svg>
-                <input type="text"/>
-              </div>
-              <div>
-                <ul>
-                  <li>1H</li>
-                  <li>24H</li>
-                  <li>1W</li>
-                  <li>1M</li>
-                  <li>1Y</li>
-                </ul>
-              </div>
-            </section>
-          </div>
-          <div className="price-at">
-            <table className="asset-table">
-              <thead className="at-head">
-                <tr className="at-row">
-                  <th>#</th>
-                  <th>Name</th>
-                  <th>Price</th>
-                  <th>Change</th>
-                  <th>Market Cap</th>
-                  <th>Trade</th>
-                </tr>
-              </thead>
-              <tbody className="at-body">
-                <tr>
-                  <td>1</td>
-                  <td>Bitcoin</td>
-                  <td>$10,634</td>
-                  <td>- 0.2%</td>
-                  <td>$189B</td>
-                  <td><button>Trade</button></td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>Ethereum</td>
-                  <td>$176</td>
-                  <td>- 0.5%</td>
-                  <td>$18.5B</td>
-                  <td><button>Trade</button></td>
-                </tr>
-                <tr>
-                  <td>3</td>
-                  <td>XRP</td>
-                  <td>$0.25</td>
-                  <td>- 1.79%</td>
-                  <td>$11B</td>
-                  <td><button>Trade</button></td>
-                </tr>
-                <tr>
-                  <td>4</td>
-                  <td>Litecoin</td>
-                  <td>$67</td>
-                  <td>- 2.82%</td>
-                  <td>~~~~~</td>
-                  <td><button>Trade</button></td>
-                </tr>
-              </tbody>
-            </table>
+          <div className="price-inner">
+            <div className="search-box">
+              <section>
+                <div>
+                  <svg></svg>
+                  <input type="text"/>
+                </div>
+                <div>
+                  <ul>
+                    <li>1H</li>
+                    <li>24H</li>
+                    <li>1W</li>
+                    <li>1M</li>
+                    <li>1Y</li>
+                  </ul>
+                </div>
+              </section>
+            </div>
+            <div className="price-at">
+              <table className="asset-table">
+                <thead className="at-head">
+                  <tr className="at-row">
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Price</th>
+                    <th>Change</th>
+                    <th>Market Cap</th>
+                    <th>Trade</th>
+                  </tr>
+                </thead>
+                <tbody className="at-body">
+                  <tr>
+                    <td>1</td>
+                    <td>Bitcoin</td>
+                    <td>$10,634</td>
+                    <td>- 0.2%</td>
+                    <td>$189B</td>
+                    <td><button>Trade</button></td>
+                  </tr>
+                  <tr>
+                    <td>2</td>
+                    <td>Ethereum</td>
+                    <td>$176</td>
+                    <td>- 0.5%</td>
+                    <td>$18.5B</td>
+                    <td><button>Trade</button></td>
+                  </tr>
+                  <tr>
+                    <td>3</td>
+                    <td>XRP</td>
+                    <td>$0.25</td>
+                    <td>- 1.79%</td>
+                    <td>$11B</td>
+                    <td><button>Trade</button></td>
+                  </tr>
+                  <tr>
+                    <td>4</td>
+                    <td>Litecoin</td>
+                    <td>$67</td>
+                    <td>- 2.82%</td>
+                    <td>~~~~~</td>
+                    <td><button>Trade</button></td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>       
           </div>
         </div>
       </div>
